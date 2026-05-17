5e5c14ec452a74fee2a50d7fe72e8e01717da3cb
fifty <fifty@nimeshdesai.name>
2026-05-17 18:58:20 +1000

    Fixed a typo in GameButtons

--- Source/Engine/Input System/GameButtons.h
+++ Source/Engine/Input System/GameButtons.h
@@ -16,8 +16,8 @@ enum class GameButtons : uint32_t {
     ActionNorth = 0x0008, // Xbox Y / PS Triangle
 
     // 2. Index Finger Shoulders
-    BumperLeft = 0x0010, // Xbox LB / PS L1 
-    BumperRight = 0x0020, // Xbox RB / PS R1
+    BumperLeft = 0x0010, // Xbox LT / PS L1 
+    BumperRight = 0x0020, // Xbox RT / PS R1
 
     // 3. Stick Click Modifiers (Physical Analogs as buttons)
     StickClickLeft = 0x0040, // Xbox LS  / PS L3
