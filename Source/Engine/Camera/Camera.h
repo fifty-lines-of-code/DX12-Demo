0171010bd9f7cfec34aafcb80e4c6d03e07fc513
fifty <fifty@nimeshdesai.name>
2026-05-18 21:49:27 +1000

    Rotating the camera around the player when moving right joystick.

--- Source/Engine/Camera/Camera.h
+++ Source/Engine/Camera/Camera.h
@@ -9,7 +9,7 @@ public:
 	~Camera();
 
 	void Initialize(DirectX::XMFLOAT4 playerPosition);
-	void Update(const DirectX::XMFLOAT4& playerPosition, float deltaTime);
+	void Update(const DirectX::XMFLOAT4& playerPosition, float deltaTime, float rightJoystickX, float rightJoystickY);
 	const DirectX::XMFLOAT4X4* GetViewProjection() const;
 
 	void OnResize(UINT newClientWidth, UINT newClientHeight);
@@ -20,6 +20,14 @@ private:
 	float mNearPlane = 1.0;
 	float mFarPlane = 1000.f;
 
+	float mYaw = 0.01;
+	float mPitch = DirectX::XMConvertToRadians(30);
+	float mPitchMin = DirectX::XMConvertToRadians(-1);
+	float mPitchMax = DirectX::XMConvertToRadians(45);
+	float mRadius = 4.f;
+	float mYawSpeed = .25f;
+	float mPitchSpeed = .25f;
+
 	DirectX::XMFLOAT4 mCenter = DirectX::XMFLOAT4(0, 2, -5.0, 1.f);
 	DirectX::XMFLOAT4 mTarget = DirectX::XMFLOAT4(0, 0, 0, 1.f);
 	DirectX::XMFLOAT4 mUp = DirectX::XMFLOAT4(0, 1, 0, 1.f);
@@ -27,10 +35,10 @@ private:
 	DirectX::XMFLOAT4X4 mProjection = MathHelper::Identity4x4();
 	DirectX::XMFLOAT4X4 mView = MathHelper::Identity4x4();
 	DirectX::XMFLOAT4X4 mViewProjection = MathHelper::Identity4x4();
-	DirectX::XMFLOAT3 mTrackingOffset;
 	const float mTrackingSpeed = 4.f;
 
 private:
+	void UpdateCenter(float deltaTime, float rightJoystickX, float rightJoystickY);
 	void BuildViewMatrix();
 	void BuildProjectionMatrix();
 	void BuildViewProjectionMatrix();
